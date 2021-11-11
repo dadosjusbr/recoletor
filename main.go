ef4381f3a832f9a4070b9c4f8dcc49b2afb53a93
Manuel-Antunes <nerdbroz2@gmail.com>
2021-11-11 18:18:23 -0300

    fix: solve all proposed issues

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -16,20 +17,16 @@ import (
 )
 
 type config struct {
-	MongoURI       string `envconfig:"MONGODB_URI" required:"true"`
-	MongoDBName    string `envconfig:"MONGODB_NAME" required:"true"`
-	MongoMICol     string `envconfig:"MONGODB_MICOL" required:"true"`
-	MongoAgCol     string `envconfig:"MONGODB_AGCOL" required:"true"`
-	MongoPkgCol    string `envconfig:"MONGODB_PKGCOL" required:"true"`
-	SwiftUsername  string `envconfig:"SWIFT_USERNAME" required:"true"`
-	SwiftAPIKey    string `envconfig:"SWIFT_APIKEY" required:"true"`
-	SwiftAuthURL   string `envconfig:"SWIFT_AUTHURL" required:"true"`
-	SwiftDomain    string `envconfig:"SWIFT_DOMAIN" required:"true"`
-	SwiftContainer string `envconfig:"SWIFT_CONTAINER" required:"true"`
+	MongoURI    string `envconfig:"MONGODB_URI" required:"true"`
+	MongoDBName string `envconfig:"MONGODB_NAME" required:"true"`
+	MongoMICol  string `envconfig:"MONGODB_MICOL" required:"true"`
+	MongoAgCol  string `envconfig:"MONGODB_AGCOL" required:"true"`
+	MongoPkgCol string `envconfig:"MONGODB_PKGCOL" required:"true"`
+	Month       string `envconfig:"MONTH" required:"true"`
+	Year        string `envconfig:"YEAR" required:"true"`
+	OutDir      string `envconfig:"OUTPUT_FOLDER"`
+	Agency      string `envconfig:"COURT" required:"true"`
 }
-
-var conf config
-var client *storage.Client
 
 func newClient(c config) (*storage.Client, error) {
 	if c.MongoMICol == "" || c.MongoAgCol == "" {
@@ -40,8 +37,7 @@ func newClient(c config) (*storage.Client, error) {
 		return nil, fmt.Errorf("error creating DB client: %q", err)
 	}
 	db.Collection(c.MongoMICol)
-	bc := storage.NewCloudClient(conf.SwiftUsername, conf.SwiftAPIKey, conf.SwiftAuthURL, conf.SwiftDomain, conf.SwiftContainer)
-	client, err := storage.NewClient(db, bc)
+	client, err := storage.NewClient(db, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating storage.client: %q", err)
 	}
@@ -49,26 +45,26 @@ func newClient(c config) (*storage.Client, error) {
 }
 
 func main() {
+	var conf config
 	godotenv.Load()
-	if err := envconfig.Process("dadosjusbr-recoletor", &conf); err != nil {
+	if err := envconfig.Process("remuneracao-magistrados", &conf); err != nil {
 		log.Fatal(err)
 	}
-	c, err := newClient(conf)
+	client, err := newClient(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	client = c
-	if _, err := strconv.Atoi(os.Getenv("MONTH")); err != nil {
-		log.Fatalf("Invalid month (\"%s\"): %q", os.Getenv("MONTH"), err)
+	if _, err := strconv.Atoi(conf.Month); err != nil {
+		log.Fatalf("Invalid month (\"%s\"): %q", conf.Month, err)
 	}
-	month, _ := strconv.Atoi(os.Getenv("MONTH"))
+	month, _ := strconv.Atoi(conf.Month)
 
-	if _, err := strconv.Atoi(os.Getenv("YEAR")); err != nil {
-		log.Fatalf("Invalid year (\"%s\"): %q", os.Getenv("YEAR"), err)
+	if _, err := strconv.Atoi(conf.Year); err != nil {
+		log.Fatalf("Invalid year (\"%s\"): %q", conf.Year, err)
 	}
-	year, _ := strconv.Atoi(os.Getenv("YEAR"))
+	year, _ := strconv.Atoi(conf.Year)
 
-	outputFolder := os.Getenv("OUTPUT_FOLDER")
+	outputFolder := conf.OutDir
 	if outputFolder == "" {
 		outputFolder = "./output"
 	}
@@ -81,21 +77,21 @@ func main() {
 	if court == "" {
 		log.Fatalf("Environment variable COURT is mandatory")
 	}
-	downloads, err := savePackage(year, month, court, outputFolder)
+	oma, _, err := client.GetOMA(month, year, court)
+	if err != nil {
+		log.Fatalf("error fetching data: %q", err)
+	}
+	downloads, err := savePackage(oma.Backups, outputFolder)
 	if err != nil {
 		log.Fatalf("error while collecting data: %q", err)
 	}
 	fmt.Println(strings.Join(downloads, "\n"))
 }
 
-func savePackage(year, month int, agency string, outDir string) ([]string, error) {
-	oma, _, err := client.GetOMA(month, year, agency)
-	if err != nil {
-		return nil, fmt.Errorf("error fetching data: %v", err)
-	}
+func savePackage(backups []storage.Backup, outDir string) ([]string, error) {
 	var files []string
-	for _, file := range oma.Backups {
-		if err := download(file.Hash, file.URL); err != nil {
+	for _, file := range backups {
+		if err := download(fmt.Sprintf("%s/%s", outDir, path.Base(file.URL)), file.URL); err != nil {
 			return nil, fmt.Errorf("error while dowloading file %s: %q", file, err)
 		}
 		files = append(files, file.URL)
